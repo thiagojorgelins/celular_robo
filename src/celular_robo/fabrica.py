1e8cdb34559a21dde68962ae1fbfc8eb6c4a8fdb
Thiago Lins <thiago.jorge2001@gmail.com>
2026-09-13 23:27:17 -0300

    feat: implementa funções para criação de robôs coletores e configurados com validação

--- src/celular_robo/fabrica.py
+++ src/celular_robo/fabrica.py
@@ -10,3 +10,34 @@
 # assinatura abaixo sem também atualizar aquele arquivo):
 #
 #   criar_robo_configurado(tipo_nome, nome, estrategia_nome=..., area_nome=...)
+from celular_robo.fabrica_base import criar_robo
+from celular_robo.estrategias import RotaColeta
+from celular_robo.modos import ModoColetando, MonitorBandeja
+from celular_robo.observadores import EquipeDeTestes, RegistroAuditoria
+from celular_robo.modelo_features import validar_configuracao, OBSTACULOS_POR_AREA
+
+
+def criar_robo_coletor(tipo_nome, nome, estrategia_nome, area_nome, **kwargs):
+    rota_classe = RotaColeta._registro_rotas[estrategia_nome]
+    obstaculos = set(OBSTACULOS_POR_AREA.get(area_nome, set()))
+    robo = criar_robo(
+        tipo_nome, nome,
+        estrategia=rota_classe(),
+        modo=ModoColetando(),
+        obstaculos=obstaculos,
+        **kwargs,
+    )
+    robo.estrategia_nome = estrategia_nome
+    robo.area_nome = area_nome
+
+    robo.adicionar_observador(MonitorBandeja())
+    robo.equipe_de_testes = EquipeDeTestes()
+    robo.adicionar_observador(robo.equipe_de_testes)
+    robo.registro_auditoria = RegistroAuditoria()
+    robo.adicionar_observador(robo.registro_auditoria)
+    return robo
+
+
+def criar_robo_configurado(tipo_nome, nome, estrategia_nome, area_nome, **kwargs):
+    validar_configuracao(tipo_nome, estrategia_nome, area_nome)
+    return criar_robo_coletor(tipo_nome, nome, estrategia_nome, area_nome, **kwargs)
